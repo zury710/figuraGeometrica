4b98a5697db737e5962c3c9ef702e67443b62e14
User <lucia.loort@ug.edu.ec>
2025-05-09 08:47:50 -0500

    commit

--- color.py
+++ color.py
@@ -1,12 +1,18 @@
-class color:
+class Color:
     def __init__(self, nombre):
-        self._nombre = nombre
+        self._color = nombre
 
-    def __init__(self)
-        return f'color: {self.__dict__. str__()}'
+    def __str__(self):
+        return f'Color: {self.__dict__.__str__()}'
+
     @property
     def nombre(self):
-        return self._nombre
+        return self._color
 
+    @nombre.setter
+    def nombre(self, nombre):
+        self._color = nombre
 
-    @
+if __name__ == '__main__':
+    c1 = Color('Rojo')
+    print(c1)
--- rectangulo.py
+++ rectangulo.py
@@ -1,8 +1,16 @@
 from FiguraGeometrica import FiguraGeometrica
+from color import Color
 
-class Rectangulo(FiguraGeometrica):
-    def __init__(self, alto, ancho):
-        super().__init__(alto, ancho)
+class Rectangulo(FiguraGeometrica, Color):
+    def __init__(self, alto=0, ancho=0, color=None):
+        FiguraGeometrica.__init__(self, alto, ancho)
+        Color.__init__(self,color)
 
-    def area(self):
-        return self._alto * self._ancho
+    def __str__(self):
+        return f"Rectángulo -> {self.__dict__.__str__()}"
+
+if __name__ == "__main__":
+    r1 = Rectangulo(alto=5,ancho=3, color="Rojo")
+    print(r1)
+    print(f'Area: {r1.area()}')
+    print(f'Perimetro: {r1.perimetro()}')
